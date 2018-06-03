50f441ccc2c18ab0c084f51977d225feebbc6752
Terrance Williams <terrance.d.williams1976@gmail.com>
2018-06-03 23:08:10 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,16 @@
 def oxford_comma(array)
  case array.length 
-	when 1
-  "#{array[0]}"
-  when 2
-  array[0..1].join(" and ")
-else 
-  array[0...-1].join(", ") << ", and #{array[-1]}"
-  end 
-end
+  
+ 
+ 
+ 
+ 
+ 
+# 	when 1
+#   "#{array[0]}"
+#   when 2
+#   array[0..1].join(" and ")
+# else 
+#   array[0...-1].join(", ") << ", and #{array[-1]}"
+#   end 
+# end
